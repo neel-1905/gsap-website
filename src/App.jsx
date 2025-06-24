eb879db03bf778d3fb53937bac2b86238a3bd1b3
neel-1905 <neelnarwadkar@gmail.com>
2025-06-24 15:42:17 +0530

    hero animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import { Hero, Navbar } from "./components";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-3xl">GSAP APPLICATION</h1>
-    </div>
+    <main>
+      <Navbar />
+      <Hero />
+
+      <div className="h-dvh bg-black"></div>
+    </main>
   );
 };
 

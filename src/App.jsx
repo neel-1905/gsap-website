4af127c12a9bf7240f5e1dc0beada18d185a74f8
neel-1905 <neelnarwadkar@gmail.com>
2025-06-25 13:11:12 +0530

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import { Hero, Navbar } from "./components";
+import { About, Hero, Navbar } from "./components";
 import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -11,7 +11,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Cocktails />
-      <div className="h-dvh bg-black"></div>
+      <About />
     </main>
   );
 };

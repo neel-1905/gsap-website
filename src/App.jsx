da498a47d3302d68e055d682e69247e3a89a9898
neel-1905 <neelnarwadkar@gmail.com>
2025-06-30 18:26:47 +0530

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
-import { About, Art, Hero, Menu, Navbar } from "./components";
+import { About, Art, Contact, Hero, Menu, Navbar } from "./components";
 import Cocktails from "./components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
@@ -14,6 +14,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
+      <Contact />
     </main>
   );
 };
